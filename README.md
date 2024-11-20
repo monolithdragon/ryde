656d14d1cee19ccbc0f2a63df2ae97e524a29a80
Joó Lajos <88922090+monolithdragon@users.noreply.github.com>
2024-11-20 20:33:15 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Ryde
+
+A full-stack Uber Clone Application with Expo’s latest features and lightning-fast edge-ready Postgres database in React Native.
---
+++ src/app/index.tsx
@@ -0,0 +1,10 @@
+import { Text } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+export default function Home() {
+  return (
+    <SafeAreaView className='items-center justify-center flex-1 bg-white'>
+      <Text>Ryde</Text>
+    </SafeAreaView>
+  );
+}
